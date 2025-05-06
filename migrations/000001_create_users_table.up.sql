97483cadc1e7456cab7035c0af2908d7023be9b2
bekhuli <bbbbek37@gmail.com>
2025-05-06 13:00:03 +0500

    fix db

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     phone TEXT NOT NULL UNIQUE,
-    full_name TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    password TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
