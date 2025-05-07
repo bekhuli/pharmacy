8a76c717f8e6ec8e71aecd0077d3930316db892c
bekhuli <bbbbek37@gmail.com>
2025-05-07 13:49:25 +0500

    adding is_admin to users

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS users (
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL DEFAULT now()
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
